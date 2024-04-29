35c4ca5c3da47aa944333244cb630d39c67f31ac
git config --global user.name tamzid620 <tamzid620@gmail.com>
2024-04-29 21:34:28 +0600

    several logo aded to skill section

--- src/components/Pages/HomeSection/MySkills/MySkills.jsx
+++ src/components/Pages/HomeSection/MySkills/MySkills.jsx
@@ -9,9 +9,10 @@ import stripe from "../../../../../src/assets/icons/stripe.png";
 import mongodb from "../../../../../src/assets/icons/mb.png";
 import expressjs from "../../../../../src/assets/icons/express.png";
 import firebase from "../../../../../src/assets/icons/firebase.png";
-import vite from "../../../../../src/assets/icons/vite.svg";
+import redux from "../../../../../src/assets/icons/redux.png";
 import axious from "../../../../../src/assets/icons/axious.png";
 import gitHub from "../../../../../src/assets/icons/github.png";
+import materialUi from "../../../../../src/assets/icons/material ui .png";
 import Aos from "aos";
 import "aos/dist/aos.css";
 
@@ -25,9 +26,9 @@ const MySkills = () => {
         My Skills
       </h2>
       <hr className="border-[#5BCCF6]"/>
-      {/* flex div  */}
+      {/* ----------------flex div------------------  */}
       <div data-aos="fade-down" className="flex justify-center mt-10 uppercase font-semibold">
-        {/* grid div  */}
+        {/* -----------------grid div -------------- */}
         <div  className=" grid sm: grid-cols-2 md:grid-cols-4 lg:grid-cols-5 gap-10">
           {/* html logo  */}
           <div 
@@ -62,15 +63,28 @@ const MySkills = () => {
               <h1 className="flex justify-center">JavaScript</h1>
             </div>
           </div>
-          {/* Node logo  */}
+          {/* MongoDb logo  */}
           <div
           data-aos="fade-down"
           data-aos-easing="linear"
-          data-aos-duration="700"
+          data-aos-duration="1400"
            className="shadow-2xl bg-transparent border-t-2 border-slate-200 p-2 rounded-lg w-[160px] flex justify-center">
             <div className="inner-element">
-              <img className="w-[100px]" src={node} alt="" />
-              <h1 className="flex justify-center">node.js</h1>
+              <img className="w-[100px]" src={mongodb} alt="" />
+              <h1 className="flex justify-center">mongodb</h1>
+            </div>
+          </div>
+          {/* Express Js logo  */}
+          <div
+          data-aos="fade-down"
+          data-aos-easing="linear"
+          data-aos-duration="1400"
+           className="shadow-2xl bg-transparent border-t-2 border-slate-200 p-2 rounded-lg w-[160px] flex justify-center">
+            <div className="inner-element">
+              {" "}
+              <br />
+              <img className="w-[100px]" src={expressjs} alt="" /> <br />
+              <h1 className="flex justify-center">express.js</h1>
             </div>
           </div>
           {/* React logo  */}
@@ -84,15 +98,37 @@ const MySkills = () => {
               <h1 className="flex justify-center">React.js</h1>
             </div>
           </div>
-          {/* vite logo  */}
+          {/* Redux logo  */}
+          <div 
+          data-aos="fade-down"
+          data-aos-easing="linear"
+          data-aos-duration="700"
+          className="shadow-2xl bg-transparent border-t-2 border-slate-200 p-2 rounded-lg w-[160px] flex justify-center">
+            <div className="inner-element">
+              <img className="w-[100px]" src={redux} alt="" />
+              <h1 className="flex justify-center">Redux</h1>
+            </div>
+          </div>
+          {/* Node logo  */}
+          <div
+          data-aos="fade-down"
+          data-aos-easing="linear"
+          data-aos-duration="700"
+           className="shadow-2xl bg-transparent border-t-2 border-slate-200 p-2 rounded-lg w-[160px] flex justify-center">
+            <div className="inner-element">
+              <img className="w-[100px]" src={node} alt="" />
+              <h1 className="flex justify-center">node.js</h1>
+            </div>
+          </div>
+          {/* materialUi logo  */}
           <div
           data-aos="fade-down"
           data-aos-easing="linear"
           data-aos-duration="1200"
            className="shadow-2xl bg-transparent border-t-2 border-slate-200 p-2 rounded-lg w-[160px] flex justify-center">
             <div className="inner-element">
-              <img className="w-[100px]" src={vite} alt="" />
-              <h1 className="flex justify-center">vite</h1>
+              <img className="w-[100px]" src={materialUi} alt="" />
+              <h1 className="flex justify-center">Material Ui</h1>
             </div>
           </div>
           {/* tailwind logo  */}
@@ -128,30 +164,6 @@ const MySkills = () => {
             <div className="inner-element">
               <img className="w-[100px]" src={stripe} alt="" /> <br />
               <h1 className="flex justify-center">stripe</h1>
-            </div>
-          </div>
-          {/* MongoDb logo  */}
-          <div
-          data-aos="fade-down"
-          data-aos-easing="linear"
-          data-aos-duration="1400"
-           className="shadow-2xl bg-transparent border-t-2 border-slate-200 p-2 rounded-lg w-[160px] flex justify-center">
-            <div className="inner-element">
-              <img className="w-[100px]" src={mongodb} alt="" />
-              <h1 className="flex justify-center">mongodb</h1>
-            </div>
-          </div>
-          {/* Express Js logo  */}
-          <div
-          data-aos="fade-down"
-          data-aos-easing="linear"
-          data-aos-duration="1400"
-           className="shadow-2xl bg-transparent border-t-2 border-slate-200 p-2 rounded-lg w-[160px] flex justify-center">
-            <div className="inner-element">
-              {" "}
-              <br />
-              <img className="w-[100px]" src={expressjs} alt="" /> <br />
-              <h1 className="flex justify-center">express.js</h1>
             </div>
           </div>
           {/* Axios logo  */}
