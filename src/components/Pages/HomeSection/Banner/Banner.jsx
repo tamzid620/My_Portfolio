4e3c8e652c13c1e991eafa2d7fbc645cf6f92940
tamzid620 <tamzid620@gmail.com>
2024-05-24 22:12:27 +0600

    final resume link adde to resume button

--- src/components/Pages/HomeSection/Banner/Banner.jsx
+++ src/components/Pages/HomeSection/Banner/Banner.jsx
@@ -50,7 +50,7 @@ const Banner = () => {
           </span>
         </p>
         <a
-          href="https://drive.google.com/file/d/1x7xJVqI3NodAyhFcz2_44h6ZHe02QHjf/view"
+          href="https://drive.google.com/file/d/1RincmeyzN1IkgezEKJms6OJPHqLrnF8_/view?usp=sharing"
           rel="noreferrer"
           target="_blank"
         >
